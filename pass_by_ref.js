53ae16fabc27f54558e84d661844b4d38c91392a
aakashmaroju <marojuaakash2003@gmail.com>
2024-09-06 00:28:06 +0530

    changes on pass by ref

--- pass_by_ref.js
+++ pass_by_ref.js
@@ -8,6 +8,7 @@ let d = c;
 
 console.log(c === d);
 
+// ***NOTE: Objects are called Reference Types
 const obj1 = {
   name: "Aakash",
 };
@@ -26,3 +27,45 @@ console.log(obj3 === obj4);
 obj4 = { id: 5 };
 console.log(obj3 === obj4); //  returns false becase upon changing the {} creat a new mem location consisting of {} and its address ie begning assigned
 // so addessses changed hence returns false;
+
+function f(value) {
+  value++;
+}
+
+let value = 10;
+
+f(value);
+
+console.log(value); /// pass by value
+
+function f(obj1) {
+  obj1.name = "Aakash";
+}
+
+let obj = {
+  name: "Prakash",
+};
+f(obj);
+console.log(obj);
+
+// Pass by reference
+
+/*
+
+when you f(obj)==> it means give obj1  = obj same as discussed above 
+but when you reassign in function then uk that it creates a new copy and starts pointing it and obj remains the same and obj1 points to newly created ones
+*/
+
+function manipulateArray(arr) {
+  arr.push(10);
+  arr = [1];
+  return arr;
+}
+
+let arr = [1, 2, 3, 5];
+
+manipulateArray(arr);
+console.log(arr); // [1,2,3,4,5]
+
+arr = manipulateArray(arr); // [1]
+console.log(arr);
