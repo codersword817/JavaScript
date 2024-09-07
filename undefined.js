83184c180567bf20cb370fbf625306c1f6209645
aakashmaroju <marojuaakash2003@gmail.com>
2024-09-08 01:16:18 +0530

    undefined, notdefined

--- undefined.js
+++ undefined.js
@@ -1,1 +1,14 @@
 // Difference betweem undefined and not defined
+
+console.log(x);
+
+// results in error because x is not defined
+
+console.log(y);
+var y; // this results in
+// hoisting in js => places a placeholder named undefined to y until it gets initialised/ assigned
+y = 10;
+console.log(y);
+
+y = undefined; /*  Striclty not a good practice  */
+console.log(y);
